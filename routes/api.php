e76cb98c8053864da312f7e7d9c30abf3f4dc2c4
Artur <arturumbelinov@gmail.com>
2025-08-08 16:55:55 -0300

    profile mais add-balance feito

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,9 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::middleware('auth:sanctum')->post('/add-balance', [AuthController::class, 'addBalance']);
+Route::middleware('auth:sanctum')->get('/profile', [AuthController::class, 'profile']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
 
